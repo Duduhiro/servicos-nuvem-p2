589bc055bbe85ae446162bb7cfebc6042b5c7144
duduhiro0312@gmail.com <duduhiro0312@gmail.com>
2025-05-01 15:44:04 -0300

    initializing back-end

---
+++ servicos-nuvem-p2-back/src/main/java/com/duduhiro/servicos_nuvem_p2_back/ServicosNuvemP2BackApplication.java
@@ -0,0 +1,13 @@
+package com.duduhiro.servicos_nuvem_p2_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicosNuvemP2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicosNuvemP2BackApplication.class, args);
+	}
+
+}
---
+++ servicos-nuvem-p2-back/src/test/java/com/duduhiro/servicos_nuvem_p2_back/ServicosNuvemP2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duduhiro.servicos_nuvem_p2_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosNuvemP2BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
