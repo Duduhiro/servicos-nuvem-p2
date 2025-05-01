c97907d3165ebd03760f1cc608d39a3f4f9956c9
duduhiro0312@gmail.com <duduhiro0312@gmail.com>
2025-05-01 20:21:34 -0300

    feat: create JwTUtil class to Webtoken

---
+++ servicos-nuvem-p2-back/src/main/java/com/duduhiro/servicos_nuvem_p2_back/Jwt/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.duduhiro.servicos_nuvem_p2_back.Jwt;
+
+import com.duduhiro.servicos_nuvem_p2_back.Entities.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long expirationMs = 8600000;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(user.getId()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public Long extractedUserId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validationToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
