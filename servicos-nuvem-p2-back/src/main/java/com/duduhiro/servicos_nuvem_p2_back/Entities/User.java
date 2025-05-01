f276ef832c98c74e3dfce5ad0a8e4ff1d28aa5cc
duduhiro0312@gmail.com <duduhiro0312@gmail.com>
2025-05-01 20:22:31 -0300

    feat: Added a constructor for the User class

--- servicos-nuvem-p2-back/src/main/java/com/duduhiro/servicos_nuvem_p2_back/Entities/User.java
+++ servicos-nuvem-p2-back/src/main/java/com/duduhiro/servicos_nuvem_p2_back/Entities/User.java
@@ -29,7 +29,11 @@ public class User {
         this.createdAt = LocalDateTime.now();
     }
 
-    public User() {}
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public Long getId() {
         return id;
