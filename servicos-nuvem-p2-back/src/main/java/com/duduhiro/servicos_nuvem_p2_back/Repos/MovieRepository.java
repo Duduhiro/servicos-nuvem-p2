ef4be7338d13f95358fe7c3b5c71aa4f90e174cb
duduhiro0312@gmail.com <duduhiro0312@gmail.com>
2025-05-01 23:36:37 -0300

    feat: Added movies fetching logic

--- servicos-nuvem-p2-back/src/main/java/com/duduhiro/servicos_nuvem_p2_back/Repos/MovieRepository.java
+++ servicos-nuvem-p2-back/src/main/java/com/duduhiro/servicos_nuvem_p2_back/Repos/MovieRepository.java
@@ -3,6 +3,13 @@ package com.duduhiro.servicos_nuvem_p2_back.Repos;
 import com.duduhiro.servicos_nuvem_p2_back.Entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+
+    Optional<Movie> findByTmdbId(Long tmdbId);
 
 }
