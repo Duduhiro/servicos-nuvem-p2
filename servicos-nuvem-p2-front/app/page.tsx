dbf4a830f2f5974b3ccfe1a2539b8c3a5c344ff4
duduhiro0312@gmail.com <duduhiro0312@gmail.com>
2025-05-08 19:05:03 -0300

    some things

--- servicos-nuvem-p2-front/app/page.tsx
+++ servicos-nuvem-p2-front/app/page.tsx
@@ -5,58 +5,67 @@ import MovieCard from "@/components/ui/movie-card";
 import MovieFeatured from "@/components/ui/movie-featured";
 import { BookmarkPlus, Search, User } from "lucide-react";
 import Link from "next/link";
+import { getPopular, getTrending } from "@/app/services/get-movies";
 
+export default async function Home() {
+  
+    // const popular = await getPopular();
+    // const trending = await getTrending();
 
-export default function Home() {
-  return (
-    <div>
-        <header className="flex justify-center border-b-[1px] border-b-white">
-            <div className="flex justify-between max-w-7xl w-full py-4">
-                <div className="flex items-center gap-2">
-                    <BookmarkPlus className="h-6 w-6" />
-                    <p className="text-2xl font-bold">WannaWatch</p>
+    return (
+        <div>
+            <header className="flex justify-center border-b-[1px] border-b-white">
+                <div className="flex justify-between max-w-7xl w-full py-4">
+                    <div className="flex items-center gap-2">
+                        <BookmarkPlus className="h-6 w-6" />
+                        <p className="text-2xl font-bold">WannaWatch</p>
+                    </div>
+                    <div className="flex items-center gap-3">
+                        <form className="relative w-full max-w-sm">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input type="search" placeholder="Search movies..." className="w-full bg-background pl-8 rounded-sm" />
+                        </form>
+                        <Link href="/to-watch">
+                            <Button variant="ghost" size="sm" className="rounded-sm">
+                                My List
+                            </Button>
+                        </Link>
+                        <Link href="/login">
+                            <Button variant="outline" size="sm" className="gap-2 rounded-sm">
+                                <User className="h-4 w-4" />
+                                Login
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
-                <div className="flex items-center gap-3">
-                    <form className="relative w-full max-w-sm">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input type="search" placeholder="Search movies..." className="w-full bg-background pl-8 rounded-sm" />
-                    </form>
-                    <Link href="/to-watch">
-                        <Button variant="ghost" size="sm" className="rounded-sm">
-                            My List
-                        </Button>
-                    </Link>
-                    <Link href="/login">
-                        <Button variant="outline" size="sm" className="gap-2 rounded-sm">
-                            <User className="h-4 w-4" />
-                            Login
-                        </Button>
-                    </Link>
+            </header>
+            <main className="flex items-center justify-center">
+                <div className="max-w-7xl w-full py-5">
+                    <h1 className="text-3xl font-bold mb-5">Featured Movies</h1>
+                    <FeaturedMovies />
+                    <h1 className="text-3xl font-bold my-5">Popular now</h1>
+                    <div className="flex start flex-wrap gap-4">
+                        {/* {
+                            popular.map(movie => {
+                                <MovieCard id={movie.id} title={movie.title} image="/placeholder.png" rating={movie.rating} watched={false} />
+                            })
+                        } */}
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                    </div>
+                    <h1 className="text-3xl font-bold my-5">Popular all time</h1>
+                    <div className="flex start flex-wrap gap-4">
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                        <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
+                    </div>
                 </div>
-            </div>
-        </header>
-        <main className="flex items-center justify-center">
-            <div className="max-w-7xl w-full py-5">
-                <h1 className="text-3xl font-bold mb-5">Featured Movies</h1>
-                <FeaturedMovies />
-                <h1 className="text-3xl font-bold my-5">Popular now</h1>
-                <div className="flex start flex-wrap gap-4">
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                </div>
-                <h1 className="text-3xl font-bold my-5">Popular all time</h1>
-                <div className="flex start flex-wrap gap-4">
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                    <MovieCard id={1} title="Dune: Part Two" image="/placeholder.png" rating={4.8} watched={true} />
-                </div>
-            </div>
-        </main>
-    </div>
-  );
+            </main>
+        </div>
+    );
 }
 
 function FeaturedMovies() {
